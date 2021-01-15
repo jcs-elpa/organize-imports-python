1bf1e7aac3486773ed54e711b7dcce9a605fe2b6
JenChieh <jcs090218@gmail.com>
2021-01-15 15:16:38 +0800

    Add core

---
+++ organize-imports-python.el
@@ -0,0 +1,42 @@
+;;; organize-imports-python.el --- Automatically organize imports in Python code.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-01-15 15:15:08
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Automatically organize imports in Python code.
+;; Keyword: organize imports python handy eclipse
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25.1"))
+;; URL: https://github.com/jcs-elpa/organize-imports-python
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Automatically organize imports in Python code.
+;;
+
+;;; Code:
+
+(defgroup organize-imports-python nil
+  "Organize imports python extension"
+  :prefix "organize-imports-python-"
+  :group 'editing
+  :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/organize-imports-python"))
+
+(provide 'organize-imports-python)
+;;; organize-imports-python.el ends here
